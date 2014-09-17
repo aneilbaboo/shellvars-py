3d1be2914eea5542e975b315b416af8ae937bc22
Aneil Mallavarapu <aneil.mallavar@gmail.com>
2014-09-17 11:54:58 -0700

    Add PyPi requirements
    Prepared for upload to PyPi.

--- setup.py
+++ setup.py
@@ -1,5 +1,28 @@
-from distutils.core import setup
+from setuptools import setup
+
+def read(f):
+    with open(f) as file:
+        return file.read()
+
 setup(name='shellvars-py',
       version='0.1.0',
+      description='Read environment variables defined in a shell script into Python.',
+      author_email='aneil.mallavar@gmail.com',
+      license='Apache2',
       py_modules=['shellvars'],
-      )
+      long_description = read('README.md'),
+      url="http://github.com/aneilbaboo/shellvars-py",
+      author="Aneil Mallavarapu",
+      include_package_data = True,
+      classifiers = [
+          'Intended Audience :: Developers',
+          'Natural Language :: English',
+          'License :: OSI Approved :: Apache Software License',
+          'Operating System :: MacOS :: MacOS X',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.6',
+          'Programming Language :: Python :: 2.7',
+          'Topic :: Software Development :: Libraries :: Python Modules'
+      ]
+)
